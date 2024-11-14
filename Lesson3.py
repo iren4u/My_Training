9c2d203d33b1578f976fa50d25c1da7eccfceb1d
iren4u <kerber.iren@gmail.com>
2024-11-14 16:15:59 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,8 @@
+kurs = "Python"
+print (kurs)
+vsego_zadach = 12
+print (vsego_zadach)
+zatracheno_chasov = 1.5
+print (zatracheno_chasov)
+print (zatracheno_chasov / vsego_zadach)
+print ("Курс:", kurs, "всего задач:",vsego_zadach, 'затрачено часов:',zatracheno_chasov, 'среднее время выполнения',(zatracheno_chasov / vsego_zadach) )
